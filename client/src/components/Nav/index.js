3abde93799e830e674dc0c3523e94dbf112e98be
rich wilk <richardwilkinson@live.com>
2021-03-11 15:57:45 -0500

    Update index.js

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,11 +2,20 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">Logo</a>
+      <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
+  <ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#!">three</a></li>
+    <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+  </ul>
+    </div>
+  </nav>
   );
 }
 
