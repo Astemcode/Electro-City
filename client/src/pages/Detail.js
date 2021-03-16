7be1f080194baf5ddf2328edcdc5423919e27742
rich wilk <richardwilkinson@live.com>
2021-03-16 19:31:44 -0400

    phone Number :

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,14 +5,14 @@ import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 
 function Detail(props) {
-  const [book, setBook] = useState({})
+  const [post, setPost] = useState({})
 
   // When this component mounts, grab the book with the _id of props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   const {id} = useParams()
   useEffect(() => {
-    API.getBook(id)
-      .then(res => setBook(res.data))
+    API.getPost(id)
+      .then(res => setPost(res.data))
       .catch(err => console.log(err));
   }, [])
 
@@ -22,7 +22,7 @@ function Detail(props) {
           <Col size="md-12">
             <Jumbotron>
               <h1>
-                {book.title} by {book.author}
+                {post.name} by {post.seller}
               </h1>
             </Jumbotron>
           </Col>
@@ -30,16 +30,19 @@ function Detail(props) {
         <Row>
           <Col size="md-10 md-offset-1">
             <article>
-              <h1>Synopsis</h1>
+              <h1>Details</h1>
               <p>
-                {book.synopsis}
+                {post.description}
+              </p>
+              <p>
+                Phone Number : 
               </p>
             </article>
           </Col>
         </Row>
         <Row>
           <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
+            <Link to="/">← Back to Search</Link>
           </Col>
         </Row>
       </Container>
