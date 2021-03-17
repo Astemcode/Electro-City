d10c5aea1f592c48a905c914ee61650ba99bf34c
Joivre Charles <astemcode@gmail.com>
2021-03-16 20:23:48 -0400

    Update Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Recaptcha from "react-recaptcha";
 import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
@@ -17,6 +18,43 @@ function Detail(props) {
       .catch(err => console.log(err));
   }, [])
 
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSubscribe = this.handleSubscribe.bind(this);
+    this.recaptchaLoaded = this.recaptchaLoaded.bind(this);
+    this.verifyCallback = this.verifyCallback.bind(this);
+
+    this.state = {
+      isVerified: false
+    }
+  }
+
+  // A verification that informs us that recaptcha has loaded properly and this function is going to be called 
+  recaptchaLoaded() {
+    console.log('capcha successfully loaded');
+  }
+
+  handleSubscribe() {
+    if (this.state.isVerified) {
+      alert('You have successfully subscribed!');
+    } else {
+      alert('Please verify that you are a human!');
+    }
+  }
+
+  // This callback function is called once the user successfully verifies themselves  
+  verifyCallback(response) {
+    if (response) {
+      this.setState({
+        isVerified: true
+      })
+    }
+  }}
+
+  render()
   return (
       <Container fluid>
   <div class="row">
@@ -29,6 +67,15 @@ function Detail(props) {
           <p> 
             Phone Number : 
           </p>
+
+          <Recaptcha
+            sitekey="6Le5aHQaAAAAAM4YFOcYK7kOVL8lORMfM80Ajvec"
+            render="explicit"
+            theme="dark"
+            onloadCallback={this.recaptchaLoaded}
+            verifyCallback={this.verifyCallback}
+          />
+
         </div>
         <div class="card-action">
           <a href="/search">Back to results</a>
